0dd47c93ee23d1159f16cfeefb02749e86765e93
JerryMiao22 <ChMiao@s.sfusd.edu>
2019-07-01 13:14:50 -0700

    Create project

---
+++ desktop/src/com/jerry/mario/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package com.jerry.mario.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.jerry.mario.Mario;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new Mario(), config);
+	}
+}
